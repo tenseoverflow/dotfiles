b57d7a4495f5042794569e022127f65501e88a1b
tenseoverflow <contact@hen.ee>
2025-09-14 18:52:58 +0300

    besh

---
+++ .config/nvim.bak/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "navarasu/onedark.nvim", lazy = true, opts = {
+    style = "dark",
+  } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
